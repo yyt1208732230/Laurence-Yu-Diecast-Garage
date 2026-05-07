e52297edfc00cfd9ee7605041311d0a4bc0b51fe
Laurence Yu <1208732230@qq.com>
2026-05-07 15:44:58 +1000

    fix bug

---
+++ node_modules/@oai/artifact-tool/dist/presentation-jsx/jsx-dev-runtime.mjs
@@ -0,0 +1,2 @@
+import { createRequire as __createRequire } from "node:module"; const require = __createRequire(import.meta.url);
+var s=Symbol.for("@oai/granola/presentation-jsx.element");var r=Symbol.for("@oai/granola/presentation-jsx.fragment");function n(e,t,...a){return o(e,t)}function o(e,t){if(typeof e==="function"){return e(t)}return{$$type:s,type:e,props:t}}export{r as Fragment,n as jsxDEV};
